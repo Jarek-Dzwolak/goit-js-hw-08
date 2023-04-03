eb7692423fa29897f8c3fbd025278c2a87fa4c00
Jarek Dzwolak <jaroslawdzwolak@gmail.com>
2023-04-03 20:42:57 +0200

    add JS and html

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,5 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-console.log('dziala');
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const MyGallery = document.querySelector('.gallery');
+
+galleryItems.forEach(image => {
+  const imageItem = document.createElement('li');
+  MyGallery.append(imageItem);
+
+  imageItem.insertAdjacentHTML(
+    'afterend',
+    `<a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>`
+  );
+});
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 200,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
+import throttle from 'lodash.throttle';
 
+const MyvideoEl = document.getElementById('vimeo-player');
+const player = new Player(MyvideoEl);
+
+player.on(
+  'timeupdate',
+  _.throttle(() => {
+    localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
+  }, 1000)
+);
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
